ed049e42b538a844cf1e6bd32961514c35e0f92c
Josue <jo.cofrethomas03@icloud.com>
2026-09-24 13:31:44 -0300

    Corrige deposito.js (scope y saldo) y textos de Deposito.html

--- Assets/Js/deposito.js
+++ Assets/Js/deposito.js
@@ -1,47 +1,6 @@
-$(document).ready(function () {
-
-    let saldo = 1250000;
-
-    $("#btnTransferir").click(function () {
-
-        let monto = parseInt($("#monto").val());
-
-        if (isNaN(monto) || monto <= 0) {
-            alert("Ingrese un monto válido");
-            return;
-        }
-
-        if (monto > saldo) {
-            alert("❌ Transacción no fue posible. Saldo insuficiente.");
-            return;
-        }
-
-        saldo -= monto;
-
-        $("#saldoDisponible").text(
-            "$" + saldo.toLocaleString("es-CL")
-        );
-
-        alert("✅ Transacción exitosa");
-        
-        $("#monto").val("");
-        $("#descripcion").val("");
-
-    });
-
-});
-/*CUANDO DEPOSITO ES EXITOSO*/
-let movimientos = JSON.parse(localStorage.getItem("movimientos")) || [];
-
-movimientos.push({
-    tipo: "Depósito",
-    fecha: new Date().toLocaleDateString("es-CL"),
-    monto: monto
-});
-
-localStorage.setItem("movimientos", JSON.stringify(movimientos));
 
 /* ESTE ES MI ACTULIZACION DE SALDO AL DEPOSITAR*/
+
 $(document).ready(function () {
 
     let saldo = parseInt(localStorage.getItem("saldo")) || 1250000;
@@ -86,9 +45,9 @@ $(document).ready(function () {
   $(document).ready(function () {
 
     let saldo =
-        parseInt(localStorage.getItem("saldoDisponible")) || 1250000;
+        parseInt(localStorage.getItem("saldo")) || 1250000;
 
-    $("#saldo").text(
+    $("#saldoDisponible").text(
         "$" + saldo.toLocaleString("es-CL")
     );
 
